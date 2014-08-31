14ef8277c4377204e8c62e429c6ec8b65d7488a2
Kenneth G. Franqueiro <kenneth.franqueiro@gmail.com>
2014-08-31 15:22:07 -0400

    Add support for alwaysAddKey option (defaults to false)
    This changes the default behavior of metalsmith-more to not add a
    `less` key for files that do not contain `<!-- more -->`.

--- test/index.js
+++ test/index.js
@@ -7,17 +7,34 @@ define([
 	'intern/dojo/node!metalsmith-more',
 	'intern/dojo/node!rimraf'
 ], function (test, assert, fs, path, metalsmith, more, rimraf) {
+	var customTagRx = /\s*<!--test-->/;
+
 	function assertResults(expectedDir, files, moreOptions) {
+		moreOptions = moreOptions || {};
 		var expectedFiles = fs.readdirSync(expectedDir),
-			key = moreOptions && moreOptions.key || 'less';
+			key = moreOptions.key || more.defaults.key;
 
 		expectedFiles.forEach(function (file) {
-			assert.strictEqual(files[file][key].toString('utf8'),
-				fs.readFileSync(path.resolve(expectedDir, file), 'utf8'));
+			var expectedText = fs.readFileSync(path.resolve(expectedDir, file), 'utf8'),
+				regexp = 'regexp' in moreOptions ? customTagRx : more.defaults.regexp;
+
+			// To allow using the same fixtures for both alwaysAddKey values,
+			// determine which assertion to run based on whether tag appears in contents
+			if (regexp.test(files[file].contents) || moreOptions.alwaysAddKey) {
+				assert.strictEqual(files[file][key].toString('utf8'),
+					expectedText);
+			}
+			else {
+				assert.isFalse(key in files[file],
+					'key should not exist if tag not present and alwaysAddKey not true');
+			}
 		});
 	}
 
-	function createTest(dir, moreOptions, additionalAssertions) {
+	function createTest(dir, moreOptions, alwaysAddKey, additionalAssertions) {
+		if (typeof alwaysAddKey !== 'undefined') {
+			moreOptions.alwaysAddKey = alwaysAddKey;
+		}
 		return function () {
 			var dfd = this.async(1000);
 			metalsmith('test/fixtures/' + dir).use(more(moreOptions)).build(dfd.callback(function (error, files) {
@@ -38,7 +55,7 @@ define([
 		assert.isFalse('less' in files['hello.html']);
 	}
 
-	test.suite('metalsmith-more', function () {
+	function createSuite(alwaysAddKey) {
 		test.afterEach(function () {
 			// Remove build folders after each test
 			['defaults', 'custom-tag', 'custom-ext'].forEach(function (dir) {
@@ -46,16 +63,25 @@ define([
 			});
 		});
 
-		test.test('default behavior', createTest('defaults'));
-		test.test('custom key', createTest('defaults', { key: 'test' }));
-		test.test('custom regexp', createTest('custom-tag', { regexp: /\s*<!--test-->/ }));
-		test.test('custom regexp as string', createTest('custom-tag', { regexp: '\\s*<!--test-->' }));
+		test.test('defaults', createTest('defaults', {}, alwaysAddKey));
+		test.test('custom key', createTest('defaults', { key: 'test' }, alwaysAddKey));
+		test.test('custom regexp',
+			createTest('custom-tag', { regexp: customTagRx }, alwaysAddKey));
+		test.test('custom regexp as string',
+			createTest('custom-tag', { regexp: customTagRx.toString().slice(1, -1) }, alwaysAddKey));
 		test.test('custom ext',
-			createTest('custom-ext', { ext: 'htm' }, customExtAssertions));
+			createTest('custom-ext', { ext: 'htm' }, alwaysAddKey, customExtAssertions));
 		test.test('custom ext (with period)',
-			createTest('custom-ext', { ext: '.htm' }, customExtAssertions));
-		test.test('utf8 content', createTest('utf8'));
+			createTest('custom-ext', { ext: '.htm' }, alwaysAddKey, customExtAssertions));
+		test.test('utf8 content', createTest('utf8', {}, alwaysAddKey));
+	}
 
-		// TODO: add fixtures and tests for custom ext and regexp
+	test.suite('metalsmith-more', function () {
+		test.test('defaults (no options)', createTest('defaults'));
+		createSuite(false);
+	});
+
+	test.suite('metalsmith-more (alwaysAddKey: true)', function () {
+		createSuite(true);
 	});
 });
